848b2004f0e7c8477d173a397b059d953164e799
Christopher Dingli <christopher@viveogroup.com>
2023-09-06 15:16:57 +1000

    Update build

--- lib/calculator.d.ts
+++ lib/calculator.d.ts
@@ -9,18 +9,11 @@ export interface ICalculateDPMQInput {
     isExtemporaneouslyPrepared?: boolean;
 }
 export declare function calculateDPMQ({ aemp, maxQuantity, packSize, isDangerousDrug, isExtemporaneouslyPrepared, brandPremium, }: ICalculateDPMQInput): number;
-export interface ICalculatePBSPriceFromAEMPInput extends ICalculateDPMQInput {
-    includeAllowableDiscount: boolean;
-    isConcessional?: boolean;
-}
-export declare function calculatePBSPriceFromAEMP({ aemp, includeAllowableDiscount, packSize, maxQuantity, isDangerousDrug, brandPremium, isExtemporaneouslyPrepared, isConcessional, }: ICalculatePBSPriceFromAEMPInput): number;
 export interface ICalculatePBSPriceInput {
     dpmq: number;
     isConcessional?: boolean;
     includeAllowableDiscount?: boolean;
     isExtemporaneouslyPrepared?: boolean;
+    isSafetyNet?: boolean;
 }
-export declare function getPBSPrice({ dpmq, isConcessional, includeAllowableDiscount, }: ICalculatePBSPriceInput): number;
-export interface ICalculateSafetyNetPriceInput extends ICalculatePBSPriceFromAEMPInput {
-}
-export declare function getSafetyNetPrice({ aemp, includeAllowableDiscount, packSize, maxQuantity, isDangerousDrug, brandPremium, isConcessional, isExtemporaneouslyPrepared, }: ICalculateSafetyNetPriceInput): void;
+export declare function getPBSPrice({ dpmq, isConcessional, isSafetyNet, includeAllowableDiscount, }: ICalculatePBSPriceInput): number;
